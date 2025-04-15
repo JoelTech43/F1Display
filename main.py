6a2ba857096050bc7ff3e17ac659307ed5df850d
Coding-Mole <joelhayward36@gmail.com>
2025-04-15 16:47:48 +0100

    HTML now displays standings, hadjar has wrong colour though

--- main.py
+++ main.py
@@ -3,14 +3,6 @@ import requests
 
 app = Flask(__name__)
 
-headings = ["Name", "Role", "Salary"] #Test data to get the html file working
-data = [
-    ["Joe Bloggs", "Software Engineer", "£42,000.00","#FF0000"],
-    ["Jane Bloggs", "Software Engineeress", "£43,000.00","#00FF00"],
-    ["Joe Boggs", "Not an Engineer", "£00,000.00","#0000FF"],
-    ["Joseph Bloggs", "Another Engineer", "£100,000.00","#00FFFF"]
-]
-
 def getChampionshipsStandings(): #requests all data and then returns needed data.
     #constructorStanding and driverStanding are the leaderboards,
     #driverData provides extra needed data like team colours.
@@ -63,11 +55,17 @@ def getChampionshipsStandings(): #requests all data and then returns needed data
 
 def getdata(): #proof of concept that I can run a function whenever the webpage is loaded.
     constructorTable, driverTable = getChampionshipsStandings()
+    constructorHeadings = constructorTable[0]
+    constructorData = constructorTable[1:]
+    driverHeadings = driverTable[0]
+    driverData = driverTable[1:]
+    return constructorHeadings, constructorData, driverHeadings, driverData
 
 @app.route("/")
 def table():
-    getdata()
+    constructorHeadings, constructorData, driverHeadings, driverData = getdata()
     timeout = 10000 #change timeout based on whether it is a session or not.
-    return render_template("tables.html", headings=headings, data=data, timeout=timeout) #renders the html file, passing it the table headers and data, and telling how long before reload.
+    return render_template("tables.html", constructorHeadings=constructorHeadings, constructorData=constructorData, driverHeadings=driverHeadings, driverData=driverData, timeout=timeout) #renders the html file, passing it the table headers and data, and telling how long before reload.
 
+print("hi")
 app.run()
